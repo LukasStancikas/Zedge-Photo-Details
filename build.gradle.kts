a9b9dca8e7a5d9536655be669d07795517b90de9
Lukas Stancikas <lukasstancikas@gmail.com>
2026-05-04 13:27:11 +0300

    Added spotless

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,4 +6,32 @@ plugins {
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.google.devtools.ksp) apply false
     alias(libs.plugins.hilt) apply false
+    alias(libs.plugins.spotless)
+}
+
+subprojects {
+    apply(plugin = "com.diffplug.spotless")
+    configure<com.diffplug.gradle.spotless.SpotlessExtension> {
+        kotlin {
+            target("**/*.kt")
+            targetExclude("**/build/**/*.kt")
+            ktlint().editorConfigOverride(
+                mapOf(
+                    "ktlint_standard_package-name" to "disabled",
+                    "ktlint_standard_function-naming" to "disabled",
+                    "ktlint_standard_no-wildcard-imports" to "disabled",
+                )
+            )
+        }
+        format("misc") {
+            target("**/*.gradle", "**/*.md", "**/.gitignore")
+            trimTrailingWhitespace()
+            endWithNewline()
+        }
+        format("xml") {
+            target("**/*.xml")
+            trimTrailingWhitespace()
+            endWithNewline()
+        }
+    }
 }
